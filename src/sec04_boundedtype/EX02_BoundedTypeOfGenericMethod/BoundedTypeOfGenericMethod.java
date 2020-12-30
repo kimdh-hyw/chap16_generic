05e3d198e5a4d88ae24ed311854d42720b07b82e
jeffrey2465 <jeffrey2465@gmail.com>
2020-12-30 20:49:45 +0900

    editing for publication

--- src/sec04_boundedtype/EX02_BoundedTypeOfGenericMethod/BoundedTypeOfGenericMethod.java
+++ src/sec04_boundedtype/EX02_BoundedTypeOfGenericMethod/BoundedTypeOfGenericMethod.java
@@ -1,8 +1,8 @@
 package sec04_boundedtype.EX02_BoundedTypeOfGenericMethod;
 
 class A {
-	public <T extends String> void method1(T t) {
-		System.out.println(t.charAt(0));
+	public <T extends Number> void method1(T t) {
+		System.out.println(t.intValue());
 	}	
 }
 
@@ -19,7 +19,7 @@ class B {
 public class BoundedTypeOfGenericMethod {
 	public static void main(String[] args) {
 		A a = new A();
-		a.method1("안녕");
+		a.method1(5.8);
 		
 		B b = new B();
 		b.method1(new MyInterface() {
